f37a6a776b774a27f13a4aa58d6f76484fb7e432
Italo <134540029+ItLrb@users.noreply.github.com>
2023-11-11 20:31:51 -0300

    final

--- discover/script.js
+++ discover/script.js
@@ -226,3 +226,4 @@ function followProfile(profileName) {
 }
 
 // DESCRIPTION MAX CHARACTERS = 136
+
--- login/login.js
+++ login/login.js
@@ -4,7 +4,6 @@ const title = document.getElementById('login-signup')
 const cfmPassBox = document.querySelectorAll('.cfm-password')[0]
 const cfmPassP = document.querySelectorAll('.cfm-password')[1]
 
-
 const loginExplain = document.getElementById('login-p')
 const signExplain = document.getElementById('signup-p')
 
@@ -18,6 +17,7 @@ function abreCadastro() {
     signExplain.style.display = 'none'
     loginExplain.style.display = 'block'
 }
+
 function abreLogar() {
     title.innerText = 'Login'
     cfmPassBox.style.display = 'none'
@@ -34,14 +34,18 @@ function doLogin() {
     const password = document.getElementById("ipassword").value
     const confirmPassword = document.getElementById('confirm-password')
 
-    
-    
+    // Verifica se o email termina com '@gmail.com'
+    if (!email.endsWith('@gmail.com')) {
+        alert("Por favor, use um endereço de e-mail válido, como '@gmail.com'.");
+        return false;
+    }
+
     if (email == "" || password == "" || confirmPassword.value == password) {
         alert("Por favor, preencha todos os campos.");
         return false;
-    }else if (email.length < 10 && password.length < 3) {
-        alert("Email ou senha inválidos, pois não atende os requisitos")
-        return false
+    } else if (email.length < 10 || password.length < 3) {
+        alert("Email ou senha inválidos, pois não atendem aos requisitos");
+        return false;
     }
 
     window.location.href = './discover.html'
